322d45a0b67c2bfc617b1bafbaf4b5b63496188e
Ayankam yared <yaredayan@gmail.com>
2021-04-27 16:29:59 +0200

    modification du composant MoviesList

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
+import {movies$} from '../Helpers/movies'
 
 export class MoviesList extends Component {
     constructor(props){
         super(props)
+        this.state = {
+            listFilms : []
+        }
     }
+
+    componentDidMount() {
+        movies$.then(movies =>{
+            this.setState({listFilms : movies})
+        })
+
+        console.log(movies$, this.state.listFilms)
+    }
+    
+
     render() {
         return (
             <h1>
-                liste films
+                liste des films : {this.state.listFilms.length}
             </h1>
         )
     }
