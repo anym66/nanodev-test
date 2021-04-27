816ce5a7493e4b1fb562fd453a7306454e99ea27
Ayankam yared <yaredayan@gmail.com>
2021-04-27 15:53:33 +0200

    creation d'un composant MoviesList

--- src/App.js
+++ src/App.js
@@ -4,32 +4,13 @@ import 'bootstrap/dist/css/bootstrap.css'
 import Buttons from './components/Buttons';
 import Count from './components/Count';
 import Search from './components/Search'
+import MoviesList from './components/MoviesList';
 
 function App() {
 
-  const [countOne, setCountOne] = useState({value : 0, btnColor : 'success', increment : 25})
-  const [countTwo, setCountTwo] = useState({value : 0, btnColor : 'danger', increment : 17 })
-
-  const incrementCountOne = useCallback( (val) => {
-    countOne.value < 100 && setCountOne({...countOne, value : countOne.value + val })
-  }, [countOne])
-
-  const incrementCountTwo = useCallback( (val) => {
-    countTwo.value < 100 && setCountTwo({...countTwo, value : countTwo.value + val })
-  }, [countTwo])
-  
   return (
     <div className="App">
-
-      <div className = "container">
-
-        <Count count = {countOne.value} bgColor = {countOne.btnColor} />
-        <Count count = {countTwo.value} bgColor = {countTwo.btnColor} />
-        <Buttons handleClick = {incrementCountOne} btnColor = {countOne.btnColor } increment = {countOne.increment} >count 1</Buttons>
-        <Buttons handleClick = {incrementCountTwo} btnColor = {countTwo.btnColor } increment = {countTwo.increment}> count 2</Buttons>
-
-      </div>
-      
+      <MoviesList />
     </div>
   );
 }
---
+++ src/components/MoviesList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class MoviesList extends Component {
+    constructor(props){
+        super(props)
+    }
+    render() {
+        return (
+            <h1>
+                liste films
+            </h1>
+        )
+    }
+}
+
+export default MoviesList
